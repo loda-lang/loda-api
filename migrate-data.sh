56f7fbd8dc58f0047a69b20ac43e5c33e8694b98
Chrstian Krause <me@ckrause.org>
2023-06-22 22:11:18 +0200

    fixes for migrate script

--- migrate-data.sh
+++ migrate-data.sh
@@ -51,6 +51,7 @@ function forbid_dir {
 ensure_dir $source_host data
 ensure_dir $source_host grafana
 ensure_dir $source_host influxdb
+forbid_dir $source_host influxdb-backup
 
 forbid_dir $target_host data
 forbid_dir $target_host grafana
@@ -60,14 +61,16 @@ echo "Creating checkpoint"
 gssh $source_host "curl -X POST localhost/miner/v1/checkpoint"
 
 echo "Creating backup"
-gssh $source_host "docker exec loda-api /usr/bin/influxd backup -portable /influx-backup"
-# TODO copy and delete backup from container
+gssh $source_host "docker exec loda-api /usr/bin/influxd backup -portable /influxdb-backup"
+gssh $source_host "docker cp loda-api:/influxdb-backup ."
+gssh $source_host "docker exec loda-api rm -R /influxdb-backup"
+ensure_dir $source_host influxdb-backup
 
 echo "Fetching data"
 gscp "$source_host:data/checkpoint.txt" .
 gscp "$source_host:data/setup.txt" .
 gscp "$source_host:grafana" .
-# gscp "$source_host:influxdb" .
+# gscp "$source_host:influxdb-backup" .
 
 echo "Pushing data"
 gssh $target_host "mkdir -p \$HOME/data"
