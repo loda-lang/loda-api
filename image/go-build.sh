921b9c9ff239a9c243bc94bdd74ce63f3e455429
Chrstian Krause <me@ckrause.org>
2024-08-23 22:13:55 +0200

    rename main

--- image/go-build.sh
+++ image/go-build.sh
@@ -7,7 +7,7 @@ export GOPATH=$HOME/go
 export PATH=$GOROOT/bin:$GOPATH/bin:$PATH
 
 pushd $GOPATH/src/github.com/loda-lang/loda-api > /dev/null
-go build -o /oeis_server cmd/oeis/oeis_server.go
-go build -o /programs_server cmd/programs/programs_server.go
-go build -o /stats_server cmd/stats/stats_server.go
+go build -o /oeis_server cmd/oeis/main.go
+go build -o /programs_server cmd/programs/main.go
+go build -o /stats_server cmd/stats/main.go
 popd > /dev/null
